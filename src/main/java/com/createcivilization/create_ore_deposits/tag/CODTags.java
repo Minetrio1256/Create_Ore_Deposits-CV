63b926da1ed4e05c7916bbe110d91e9f507440ee
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-03-03 10:37:07 -0500

    Ported the Tags

--- src/main/java/com/createcivilization/create_ore_deposits/tag/CODTags.java
+++ src/main/java/com/createcivilization/create_ore_deposits/tag/CODTags.java
@@ -1,9 +1,8 @@
 package com.createcivilization.create_ore_deposits.tag;
 
 import com.createcivilization.create_ore_deposits.CreateOreDeposits;
-
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.*;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class CODTags {
@@ -12,12 +11,11 @@ public class CODTags {
 
         public static final TagKey<Block> ORE_DEPOSITS = registerTag("ore_deposits");
 
-        //Remove This Please
-
         public static final TagKey<Block> EVERYTHING = registerTag("everything");
 
         public static TagKey<Block> registerTag(String name) {
-            return BlockTags.create(new ResourceLocation(CreateOreDeposits.MOD_ID, name));
+            return TagKey.create(net.minecraft.core.registries.Registries.BLOCK,
+                    ResourceLocation.fromNamespaceAndPath(CreateOreDeposits.MOD_ID, name));
         }
     }
 }
